9ab8682c943959d2a3893431d62327bb07a28510
sefufim <132181028+sefufim@users.noreply.github.com>
2023-08-09 18:29:21 -0700

    reworked code and added a new variable that holds all the values

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,31 +1,39 @@
-// Assignment code here
+// password criteria will hold all the values needed 
+var passwordCriteria = {
+  passwordLength:null, passwordCase:null, passwordCharacter: null, passwordNums:null
+}
+
 // passwordLength variable prompts the user and asks them for their password length
-var passwordLength= Number(prompt("how many characters would you like your passowrd to be?","Enter a number between 8 and 128 " ));
+passwordCriteria.passwordLength= Number(prompt("how many characters would you like your passowrd to be?","Enter a number between 8 and 128 " ));
 //checks to see if password length fits the in criteria boundaries
-while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+while (isNaN(passwordCriteria.passwordLength) || passwordCriteria.passwordLength < 8 || passwordCriteria.passwordLength > 128) {
   alert("Please enter a valid number between 8 and 128.");
-  passwordLength = Number(prompt("How many characters would you like your password to be? Enter a number between 8 and 128"));
+  passwordCriteria.passwordLength = Number(prompt("How many characters would you like your password to be? Enter a number between 8 and 128"));
 }
 // password nums asks the user if they like their password to contain numbers
-var passwordNums = prompt("Would you like your password to contain numbers?");
+passwordCriteria.passwordNums = prompt("Would you like your password to contain numbers?");
 // while loop checks to see if provided answers fall under the password criteria
-while (!passwordNums.includes("yes") && !passwordNums.includes("no")){
-  passwordNums = prompt("Would you like your password to contain numbers?" , "yes or no");
+while (!passwordCriteria.passwordNums.includes("yes") && !passwordCriteria.passwordNums.includes("no")){
+  passwordCriteria.passwordNums = prompt("Would you like your password to contain numbers?" , "yes or no");
 }
 // password case asks the user if they would like their password to contain uppercase, lowercase or both
-var passwordCase = prompt("Would you like your password to contain uppercase, lowercase, or both");
+passwordCriteria.passwordCase = prompt("Would you like your password to contain uppercase, lowercase, or both");
 // while loop checks to see if provided answers fall under the password criteria
-while (!passwordCase.includes("uppercase") && !passwordCase.includes("lowercase" && !passwordCase.includes("both"))){
-  passwordCase = prompt("Would you like your password to contain uppercase, lowercase, or both?");
+while (!passwordCriteria.passwordCase.includes("uppercase") && !passwordCriteria.passwordCase.includes("lowercase") && !passwordCriteria.passwordCase.includes("both")){
+  passwordCriteria.passwordCase = prompt("Would you like your password to contain uppercase, lowercase, or both?");
 }
 // passwordCharacter variable asks the user if they would like their password to contain special chars
-var passwordCharacter = prompt("would your password to include special characters?");
+passwordCriteria.passwordCharacter = prompt("would your password to include special characters?");
 // checks to see if password characters is part of the accepted criteria
-while (!passwordCharacter.includes("yes") && !passwordCharacter.includes("no")){
-  passwordCharacter = prompt("Would you like your password to special characters?" , "yes or no");
+while (!passwordCriteria.passwordCharacter.includes("yes") && !passwordCriteria.passwordCharacter.includes("no")){
+  passwordCriteria.passwordCharacter = prompt("Would you like your password to special characters?" , "yes or no");
 }
+// 
+console.log(passwordCriteria);
+
 // generatePassword 
 function generatePassword(){
+
 
 
 }
